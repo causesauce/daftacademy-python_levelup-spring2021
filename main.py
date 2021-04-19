f47e3180a5c137533e7eb377d3319663c8076932
Adrian <67865361+causesauce@users.noreply.github.com>
2021-04-19 02:21:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-@app.get("/auth/")
+from fastapi import FastAPI, status, Response
+import uvicorn
+import hashlib
+
+app = FastAPI()
+
+
+@app.get("/auth")
 def validate_password(password: str, password_hash: str, response: Response):
+
     normal_to_hashed = hashlib.sha512(password.encode()).hexdigest()
-    response.status_code = 401
-    if password_hash == normal_to_hashed:
-        response.status_code = 204
+    response.status_code = status.HTTP_401_UNAUTHORIZED
+    if password_hash == normal_to_hashed and password is not None and password_hash is not None and password != '' and password_hash != '':
+        response.status_code = status.HTTP_204_NO_CONTENT
 
     return response
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
